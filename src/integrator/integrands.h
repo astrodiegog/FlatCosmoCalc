7d6c3936f126a0de487db2958b8be6bb3f61b645
Diego Garza <diego2500garza@uchicago.edu>
2024-07-03 14:41:26 -0700

    add function descriptions

--- src/integrator/integrands.h
+++ src/integrator/integrands.h
@@ -6,23 +6,38 @@ double integrandONE(double x, double *args);
 /* Routine to help unit test integrator. Compare against sin(x) */
 double integrandTWO(double x, double *args);
 
-/*\fn double integrandTWO_exp(double)*/
+/*\fn double integrandTHREE(double)*/
 /* Routine to help unit test integrator. Compare against ln(ax+b)/a */
 double integrandTHREE(double x, double *args);
 
-
+/*\fn double integrandFOUR(double)*/
+/* Routine to help unit test integrator. */
 double integrandFOUR(double x, double *args);
 
+/*\fn double integrandFOUR_LN(double)*/
+/* Routine to help unit test integrator on logspace. Calls integrandFOUR */
 double integrandFOUR_LN(double x, double *args);
 
+/*\fn double integrandFIVE(double)*/
+/* Routine to help unit test integrator. */
 double integrandFIVE(double x, double *args);
 
+/*\fn double integrandFIVE_LN(double)*/
+/* Routine to help unit test integrator on logspace. Calls integrandFIVE */
 double integrandFIVE_LN(double x, double *args);
 
+/*\fn double integrandSIX(double)*/
+/* Routine to help unit test integrator. */
 double integrandSIX(double x, double *args);
 
+/*\fn double integrandSIX_LN(double)*/
+/* Routine to help unit test integrator on logspace. Calls integrandSIX */
 double integrandSIX_LN(double x, double *args);
 
+/*\fn double integrandSEVEN(double)*/
+/* Routine to help unit test integrator. */
 double integrandSEVEN(double x, double *args);
 
+/*\fn double integrandSEVEN_LN(double)*/
+/* Routine to help unit test integrator on logspace. Calls integrandSEVEN */
 double integrandSEVEN_LN(double x, double *args);
